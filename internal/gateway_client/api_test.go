8a3ec1558f1f19fa438a0306676e3aef90ab9f00
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-08-31 00:45:15 +0100

    moving api def tests to the api_test file

---
+++ internal/gateway_client/api_test.go
@@ -0,0 +1,29 @@
+package gateway_client
+
+import (
+	"testing"
+)
+
+func TestApi_All(t *testing.T) {
+	c := getClient()
+	apis, err := c.Api.All()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	for _, a := range apis {
+		t.Log("api:", a.APIID, a.Domain, a.Slug, a.Proxy.ListenPath)
+	}
+}
+
+func TestApi_Create(t *testing.T) {
+	t.Fatal("no test")
+}
+
+func TestApi_Update(t *testing.T) {
+	t.Fatal("no test")
+}
+
+func TestApi_Delete(t *testing.T) {
+	t.Fatal("no test")
+}
