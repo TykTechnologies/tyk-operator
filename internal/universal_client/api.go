5f94c00295fda85f87aa88debf82a1372fcfa346
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-09-23 08:17:06 +0100

    refactor / cleanup the apidef reconciler (#29)

--- internal/universal_client/api.go
+++ internal/universal_client/api.go
@@ -2,6 +2,7 @@ package universal_client
 
 import (
 	v1 "github.com/TykTechnologies/tyk-operator/api/v1"
+	"github.com/pkg/errors"
 )
 
 type UniversalApi interface {
@@ -10,4 +11,47 @@ type UniversalApi interface {
 	Create(def *v1.APIDefinitionSpec) (apiID string, err error)
 	Update(apiID string, def *v1.APIDefinitionSpec) error
 	Delete(id string) error
+}
+
+func CreateOrUpdateAPI(c UniversalClient, spec *v1.APIDefinitionSpec) (*v1.APIDefinitionSpec, error) {
+	var err error
+
+	allAPIs, err := c.Api().All()
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get all apis")
+	}
+	apiID := ""
+	for _, api := range allAPIs {
+		if spec.Proxy.ListenPath == api.Proxy.ListenPath {
+			apiID = api.APIID
+
+			// Overwrite the ORGID if found
+			spec.OrgID = api.OrgID
+			spec.APIID = api.APIID
+			break
+		}
+	}
+
+	if apiID == "" {
+		// Create
+		apiID, err = c.Api().Create(spec)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to create api")
+		}
+	} else {
+		// Update
+		err = c.Api().Update(apiID, spec)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to update api")
+		}
+	}
+
+	_ = c.HotReload()
+
+	api, err := c.Api().Get(apiID)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to get created api")
+	}
+
+	return api, nil
 }
