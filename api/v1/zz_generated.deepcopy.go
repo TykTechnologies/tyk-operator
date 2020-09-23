57e224fe9d0b737eed92a25c4a160c29c161b572
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-09-23 08:32:11 +0100

    changing apache2 to mpl2 (#30)

--- api/v1/zz_generated.deepcopy.go
+++ api/v1/zz_generated.deepcopy.go
@@ -3,11 +3,11 @@
 /*
 
 
-Licensed under the Apache License, Version 2.0 (the "License");
+Licensed under the Mozilla Public License (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+    https://www.mozilla.org/en-US/MPL/2.0/
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,6 +22,7 @@ package v1
 
 import (
 	"encoding/json"
+
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
