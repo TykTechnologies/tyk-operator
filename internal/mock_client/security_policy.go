0d3b67c7ab78393898bd282a844db517a98c4efc
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-10-01 16:32:44 +0100

    [wip] ci / cd pull request GH Actions (#41)
    MVP for operator CI

---
+++ internal/mock_client/security_policy.go
@@ -0,0 +1,27 @@
+package mock_client
+
+import v1 "github.com/TykTechnologies/tyk-operator/api/v1"
+
+type SecurityPolicy struct {
+	*Client
+}
+
+func (p SecurityPolicy) All() ([]v1.SecurityPolicySpec, error) {
+	return nil, nil
+}
+
+func (p SecurityPolicy) Get(polId string) (*v1.SecurityPolicySpec, error) {
+	return nil, nil
+}
+
+func (p SecurityPolicy) Create(def *v1.SecurityPolicySpec) (string, error) {
+	return "", nil
+}
+
+func (p SecurityPolicy) Update(def *v1.SecurityPolicySpec) error {
+	return nil
+}
+
+func (p SecurityPolicy) Delete(id string) error {
+	return nil
+}
