abfa03eaa2d67c181ddd5515d43785871ca8aad4
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-10-03 10:44:27 +0100

    fixing tests

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -30,7 +30,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	tykv1 "github.com/TykTechnologies/tyk-operator/api/v1"
+	tykv1 "github.com/TykTechnologies/tyk-operator/api/v1alpha1"
 	// +kubebuilder:scaffold:imports
 )
 
