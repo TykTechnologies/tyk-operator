ad5b0dfdec66f8a6f791cf75f34726edac899cd2
Sedky <sedkyaboushamalah@gmail.com>
2020-10-06 17:38:11 -0400

    Forgot two scaffolded files

---
+++ internal/universal_client/webhook.go
@@ -0,0 +1,33 @@
+package universal_client
+
+import (
+	tykv1alpha1 "github.com/TykTechnologies/tyk-operator/api/v1alpha1"
+	"github.com/pkg/errors"
+)
+
+type UniversalWebhook interface {
+	All() ([]tykv1alpha1.WebhookSpec, error)
+	Get(namespacedName string) (*tykv1alpha1.WebhookSpec, error)
+	Create(def *tykv1alpha1.WebhookSpec, namespacedName string) (string, error)
+	Update(def *tykv1alpha1.WebhookSpec, namespacedName string) error
+	Delete(namespacedName string) error
+}
+
+var (
+	WebhookCollisionError = errors.New("webhook already exists")
+	WebhookNotFoundError  = errors.New("webhook not found")
+)
+
+func applyDefaultsWebhooks(spec *tykv1alpha1.WebhookSpec) {
+}
+
+func CreateOrUpdateWebhook(c UniversalClient, spec *tykv1alpha1.WebhookSpec, namespacedName string) (*tykv1alpha1.WebhookSpec, error) {
+	var err error
+
+	webhook := tykv1alpha1.WebhookSpec{}
+	applyDefaultsWebhooks(spec)
+
+	_ = c.HotReload()
+
+	return &webhook, err
+}
