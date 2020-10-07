1b2082cdc99d20458e589bad834d9af3daf98c9b
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-10-07 21:04:33 +0100

    Feat/68 ci cd (#72)
    #68
    
    Foundations for unit-tests + integration tests.
    
    Deployment for Tyk Pro created.
    License key from GitHub Secret
    
    Outstanding:
    
    - [ ] Dashboard requires bootstrapping
    - [ ] Necessity to deploy CRDs to the Kind cluster
    - [ ] Test suite to run through CRUD of API Definitions + running CURL tests against them.

---
+++ ci/deploy_tyk_pro.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+NAMESPACE=tykpro-control-plane
+PRODIR=${PWD}/ci/tyk-pro
+
+echo "creating namespace ${NAMESPACE}"
+kubectl create namespace ${NAMESPACE}
+
+echo "deploying databases"
+kubectl apply -f "${PRODIR}/mongo/mongo.yaml" -n ${NAMESPACE}
+kubectl apply -f "${PRODIR}/redis" -n ${NAMESPACE}
+
+echo "waiting for redis"
+kubectl wait deployment/redis -n ${NAMESPACE} --for condition=available --timeout=60s
+echo "waiting for mongo"
+kubectl wait deployment/mongo -n ${NAMESPACE} --for condition=available --timeout=60s
+
+echo "creating configmaps"
+kubectl create configmap -n ${NAMESPACE} dash-conf --from-file "${PRODIR}/dashboard/confs/dash.json"
+kubectl create configmap -n ${NAMESPACE} tyk-conf --from-file "${PRODIR}/gateway/confs/tyk.json"
+
+echo "getting dash license key"
+echo -n "${TYK_DB_LICENSEKEY}" > ./license.txt
+kubectl create secret -n ${NAMESPACE} generic dashboard --from-file=./license.txt
+
+echo "deploying dashboard & gateway"
+kubectl apply -f "${PRODIR}/dashboard/dashboard.yaml" -n ${NAMESPACE}
+kubectl apply -f "${PRODIR}/gateway/gateway.yaml" -n ${NAMESPACE}
+
+echo "waiting for dashboard"
+kubectl wait deployment/dashboard -n ${NAMESPACE} --for condition=available
+echo "waiting for gateway"
+kubectl wait deployment/tyk -n ${NAMESPACE} --for condition=available
+
+kubectl logs svc/dashboard -n ${NAMESPACE}
