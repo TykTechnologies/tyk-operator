85ff1046c745611e1f69da4ee158709c256c328e
Sedky <sedkyaboushamalah@gmail.com>
2020-10-19 22:32:12 -0400

    Simplified the logic and reduced number of necessary reconciles AND API calls to Dashboard

--- internal/universal_client/api.go
+++ internal/universal_client/api.go
@@ -3,6 +3,8 @@ package universal_client
 import (
 	v1 "github.com/TykTechnologies/tyk-operator/api/v1alpha1"
 	"github.com/pkg/errors"
+	"os"
+	"strings"
 )
 
 type UniversalApi interface {
@@ -13,53 +15,41 @@ type UniversalApi interface {
 	Delete(id string) error
 }
 
-func CreateOrUpdateAPI(c UniversalClient, spec *v1.APIDefinitionSpec) (*v1.APIDefinitionSpec, error) {
+func CreateOrUpdateAPI(c UniversalClient, spec *v1.APIDefinitionSpec) error {
 	var err error
 
-	// should return nil, nil http.BadRequest if api doesn't exist
+	// should return nil, nil if api doesn't exist
 	api, err := c.Api().Get(spec.APIID)
 	if err != nil {
-		return nil, errors.Wrap(err, "Unable to communicate with Client")
+		return errors.Wrap(err, "Unable to communicate with Client")
 	}
 
 	if api == nil {
 		// Create
 		insertedId, err := c.Api().Create(spec)
 		if err != nil {
-			return nil, errors.Wrap(err, "unable to create api")
-		}
-		api, err = c.Api().Get(insertedId)
-		if err != nil {
-			return nil, errors.Wrap(err, "unable to get inserted api")
+			return errors.Wrap(err, "unable to create api")
 		}
 
-		// if this is an existing CRD and the "api_id" does not match between the two APIs, that means
-		// the newly created one was unintentionally deleted.  The next action updates the "api_id" to the old one in order to resync
-		if api.APIID != spec.APIID {
-			spec.OrgID = api.OrgID
-			err = c.Api().Update(api.APIID, spec)
-			if err != nil {
-				return nil, errors.Wrap(err, "unable to update api")
-			}
+		// todo: replace this once we replace it in main.go
+		spec.OrgID = strings.TrimSpace(os.Getenv("TYK_ORG"))
+
+		// Update "api_id" to preserve it
+		err = c.Api().Update(insertedId, spec)
+		if err != nil {
+			return errors.Wrap(err, "unable to update api")
 		}
 
 	} else {
 		// Update
 		spec.OrgID = api.OrgID
-		spec.ID = api.ID
-		spec.APIID = api.APIID
-		err = c.Api().Update(api.APIID, spec)
+		err = c.Api().Update(spec.APIID, spec)
 		if err != nil {
-			return nil, errors.Wrap(err, "unable to update api")
+			return errors.Wrap(err, "unable to update api")
 		}
-	}
-
-	api, err = c.Api().Get(spec.APIID)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to get final api form")
 	}
 
 	_ = c.HotReload()
 
-	return api, nil
+	return nil
 }
