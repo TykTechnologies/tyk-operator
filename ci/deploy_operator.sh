5ae2e7840bffe82ec7f67b630a63439f965ec77a
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-10-26 18:32:30 +0000

    Feat/webhook fix (#123)
    * fixing webhook configuration
    
    * using env-from
    
    * publishing GW on ports 8000 and 8443

--- ci/deploy_operator.sh
+++ ci/deploy_operator.sh
@@ -17,9 +17,9 @@ TYK_MODE=pro
 TYK_URL=http://dashboard.tykpro-control-plane.svc.cluster.local:3000
 
 kubectl create secret -n ${NAMESPACE} generic tyk-operator-conf \
-  --from-literal "tykAuth=${TYK_AUTH}" \
-  --from-literal "tykOrg=${TYK_ORG}" \
-  --from-literal "tykMode=${TYK_MODE}" \
-  --from-literal "tykURL=${TYK_URL}"
+  --from-literal "TYK_AUTH=${TYK_AUTH}" \
+  --from-literal "TYK_ORG=${TYK_ORG}" \
+  --from-literal "TYK_MODE=${TYK_MODE}" \
+  --from-literal "TYK_URL=${TYK_URL}"
 
 kubectl get secret/tyk-operator-conf -n ${NAMESPACE} -o json | jq '.data'
