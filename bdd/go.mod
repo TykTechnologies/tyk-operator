bab515048de78310e583e9ead44a1a1fa7aaa07d
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-10-30 18:27:10 +0000

    initial attempt at BDD

---
+++ bdd/go.mod
@@ -0,0 +1,5 @@
+module github.com/TykTechnologies/tyk-operator/bdd
+
+go 1.14
+
+require github.com/cucumber/godog v0.10.0
--- bdd/godogs_test.go
+++ bdd/godogs_test.go
@@ -1,25 +1,76 @@
 package main
 
 import (
-	"os/exec"
+	"errors"
 
 	"github.com/cucumber/godog"
 )
 
-func InitializeScenario(ctx *godog.ScenarioContext) {
-	ctx.Step(`^there is a (\s+) resource$`, thereIsAResource)
+type store struct {
+	responseCode int
+	cleanupK8s   []string
 }
 
-func thereIsAResource(arg1 string) error {
-	app := "kubectl"
+func InitializeScenario(ctx *godog.ScenarioContext) {
+	s := store{}
 
-	cmd := exec.Command(app, "apply", "-f", arg1)
-	_, err := cmd.Output()
+	ctx.Step(`^there is a (\S+) resource`, s.thereIsAResource)
+	ctx.Step(`^i update a (\S+) resource`, s.iUpdateAResource)
+	ctx.Step(`^i request (\S+) endpoint$`, s.iRequestEndpoint)
+	ctx.Step(`^there should be a (\d+) http response code$`, s.thereShouldBeHttpResponseCode)
+}
 
-	// TODO: do something to test that the resource was actually created
+func (s *store) iRequestEndpoint(path string) error {
+	//myCommand := []string{
+	//	"run",
+	//	"mycurl",
+	//	"-it",
+	//	"--rm",
+	//	"--image",
+	//	"radial/busyboxplus:curl",
+	//	fmt.Sprintf("http://gw.tykpro-control-plant.svc:8000%s", path),
+	//}
+	//
+	//cmd := exec.Command("kubectl", myCommand...)
+	//output, err := cmd.Output()
+	//if err != nil {
+	//	return err
+	//}
+	//println(string(output))
 
-	if err != nil {
-		return err
+	//res, err := http.Get(path)
+	//if err != nil {
+	//	return err
+	//}
+
+	s.responseCode = 200
+
+	return nil
+}
+
+func (s *store) thereIsAResource(arg1 string) error {
+	//app := "kubectl"
+	//
+	//cmd := exec.Command(app, "apply", "-f", arg1)
+	//output, err := cmd.Output()
+	//
+	//println(string(output))
+	//// TODO: do something to test that the resource was actually created
+	//
+	//if err != nil {
+	//	return err
+	//}
+
+	return nil
+}
+
+func (s *store) iUpdateAResource(arg1 string) error {
+	return s.thereIsAResource(arg1)
+}
+
+func (s *store) thereShouldBeHttpResponseCode(expectedCode int) error {
+	if expectedCode != 200 {
+		return errors.New("unexpected response code")
 	}
 
 	return nil
