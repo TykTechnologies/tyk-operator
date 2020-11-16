c17cbf44a5580fc2cd43f8e8b072f6d673530c38
tbuchaillot <tombuchaillot89@gmail.com>
2020-11-15 21:34:36 -0300

    restoring the way we wait for deploys

--- ci/deploy_tyk_pro.sh
+++ ci/deploy_tyk_pro.sh
@@ -23,8 +23,10 @@ echo "deploying databases"
 kubectl apply -f "${PRODIR}/mongo/mongo.yaml" -n ${NAMESPACE}
 kubectl apply -f "${PRODIR}/redis" -n ${NAMESPACE}
 
-echo "waiting for redis and mongo"
-kubectl wait --for=condition=available --all deployments --timeout=${TIMEOUT}
+echo "waiting for redis"
+kubectl wait deployment/redis -n ${NAMESPACE} --for condition=available --timeout=${TIMEOUT}
+echo "waiting for mongo"
+kubectl wait deployment/mongo -n ${NAMESPACE} --for condition=available --timeout=${TIMEOUT}
 
 echo "creating configmaps"
 kubectl create configmap -n ${NAMESPACE} dash-conf --dry-run=client --from-file "${PRODIR}/dashboard/confs/dash.json" -o yaml | kubectl apply -f -
@@ -36,12 +38,11 @@ kubectl get secret/dashboard -n tykpro-control-plane -o jsonpath='{.data.license
 
 echo "deploying dashboard"
 kubectl apply -f "${PRODIR}/dashboard/dashboard.yaml" -n ${NAMESPACE}
+kubectl wait deployment/dashboard -n ${NAMESPACE} --for condition=available --timeout=${TIMEOUT}
 
 echo "deploying gateway"
 kubectl apply -f "${PRODIR}/gateway/gateway.yaml" -n ${NAMESPACE}
-
-echo "waiting for gateway and dashboard"
-kubectl wait --for=condition=available --all deployments --timeout=${TIMEOUT}
+kubectl wait deployment/tyk -n ${NAMESPACE} --for condition=available --timeout=${TIMEOUT}
 
 echo "dashboard logs"
 kubectl logs svc/dashboard -n ${NAMESPACE}
