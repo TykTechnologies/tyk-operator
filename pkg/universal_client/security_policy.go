d511bae16ebc125457ff53d23c4dbbe9f424e975
gernest <geofreyernest@live.com>
2020-12-10 17:10:27 +0300

    cleanup

--- pkg/universal_client/security_policy.go
+++ pkg/universal_client/security_policy.go
@@ -2,7 +2,6 @@ package universal_client
 
 import (
 	tykv1alpha1 "github.com/TykTechnologies/tyk-operator/api/v1alpha1"
-	"github.com/pkg/errors"
 )
 
 type UniversalSecurityPolicy interface {
@@ -18,41 +17,3 @@ type UniversalSecurityPolicy interface {
 	//Delete deletes policy id id
 	Delete(id string) error
 }
-
-var (
-	PolicyCollisionError = errors.New("policy already exists")
-	PolicyNotFoundError  = errors.New("policy not found")
-)
-
-// func CreateOrUpdatePolicy(c UniversalClient, spec *tykv1alpha1.SecurityPolicySpec) (*tykv1alpha1.SecurityPolicySpec, error) {
-// 	var err error
-
-// 	pol, err := c.SecurityPolicy().Get(spec.ID)
-// 	if err != nil {
-// 		// should return "nil, http.401" if policy doesn't exist
-// 		if !errors.Is(err, PolicyNotFoundError) {
-// 			return nil, errors.Wrap(err, "Unable to communicate with Client")
-// 		}
-// 	}
-
-// 	if pol == nil {
-// 		// Create
-// 		err := c.SecurityPolicy().Create(spec)
-// 		if err != nil {
-// 			return nil, errors.Wrap(err, "unable to create policy")
-// 		}
-
-// 	} else {
-// 		// Update
-// 		spec.MID = pol.MID
-// 		spec.OrgID = pol.OrgID
-// 		err = c.SecurityPolicy().Update(spec)
-// 		if err != nil {
-// 			return nil, errors.Wrap(err, "unable to update policy")
-// 		}
-// 	}
-
-// 	_ = c.HotReload()
-
-// 	return pol, nil
-// }
