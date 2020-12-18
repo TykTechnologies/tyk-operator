47132da7fa10b758f0d26e1f205ceebfefccda4a
Ahmet Soormally <ahmet@mangomm.co.uk>
2020-12-18 12:39:27 +0000

    Ingress Support (#201)
    Co-authored-by: gernest <geofreyernest@live.com>

--- controllers/ingress_controller.go
+++ controllers/ingress_controller.go
@@ -18,21 +18,31 @@ package controllers
 
 import (
 	"context"
-	"errors"
+	"crypto/sha256"
 	"fmt"
+	"strings"
 
 	"github.com/TykTechnologies/tyk-operator/api/v1alpha1"
 	"github.com/TykTechnologies/tyk-operator/pkg/universal_client"
 	"github.com/go-logr/logr"
 	"k8s.io/api/networking/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
-const ingressFinalizerName = "finalizers.tyk.io/ingress"
+const (
+	labelKey                           = "tyk.io/ingress"
+	ingressFinalizerName               = "finalizers.tyk.io/ingress"
+	ingressClassAnnotationKey          = "kubernetes.io/ingress.class"
+	ingressTemplateAnnotationKey       = "tyk.io/template"
+	defaultIngressClassAnnotationValue = "tyk"
+)
 
 // CertificateReconciler reconciles a CertificateSecret object
 type IngressReconciler struct {
@@ -57,13 +67,37 @@ func (r *IngressReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	// ALL MANAGED API DEFINITIONS =====================================================================================
+
+	oldAPIs := v1alpha1.ApiDefinitionList{}
+	opts := []client.ListOption{
+		client.InNamespace(req.Namespace),
+		client.MatchingLabels{labelKey: req.Name},
+	}
+	if err := r.List(ctx, &oldAPIs, opts...); err != nil {
+		log.Error(err, "unable to list apis")
+		return ctrl.Result{}, err
+	}
+
+	// /MANAGED API DEFINITIONS ========================================================================================
+
 	// FINALIZER LOGIC =================================================================================================
 
 	// If object is being deleted
 	if !desired.ObjectMeta.DeletionTimestamp.IsZero() {
 		// If our finalizer is present, need to delete from Tyk still
 		if containsString(desired.ObjectMeta.Finalizers, ingressFinalizerName) {
-			// TODO: Logic to delete ALL apis created by this ingress resource
+			log.Info("resource is being deleted - removing associated api definitions")
+
+			for _, a := range oldAPIs.Items {
+				_ = r.Delete(ctx, &a)
+			}
+
+			log.Info("removing finalizer from ingress")
+			desired.ObjectMeta.Finalizers = removeString(desired.ObjectMeta.Finalizers, ingressFinalizerName)
+			if err := r.Update(ctx, desired); err != nil {
+				return ctrl.Result{}, err
+			}
 		}
 
 		return ctrl.Result{}, nil
@@ -71,6 +105,7 @@ func (r *IngressReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 
 	// If finalizer not present, add it; This is a new object
 	if !containsString(desired.ObjectMeta.Finalizers, ingressFinalizerName) {
+		log.Info("adding finalizer", "name", ingressFinalizerName)
 		desired.ObjectMeta.Finalizers = append(desired.ObjectMeta.Finalizers, ingressFinalizerName)
 		err := r.Update(ctx, desired)
 		// Return either way because the update will
@@ -80,102 +115,194 @@ func (r *IngressReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 
 	// /FINALIZER LOGIC ================================================================================================
 
-	if desired.Spec.IngressClassName == nil {
-		log.Info("IngressClassName should not be nil")
-		return ctrl.Result{}, errors.New("IngressClassName should not be nil")
-	}
-
-	ingressClassList := &v1beta1.IngressClassList{}
-	opts := []client.ListOption{
-		client.InNamespace(req.Namespace),
-	}
-	err := r.List(ctx, ingressClassList, opts...)
-	if err != nil {
-		println(err.Error())
-		return ctrl.Result{}, err
-	}
-
-	println(ingressClassList.Kind)
-
-	ingressClass := &v1beta1.IngressClass{}
-	if err := r.Get(ctx, types.NamespacedName{Name: *desired.Spec.IngressClassName, Namespace: req.Namespace}, ingressClass); err != nil {
-		println(err.Error())
-		return ctrl.Result{}, err
-	}
-
-	if ingressClass.Spec.Controller != "tyk-operator" {
-		log.Info(fmt.Sprintf("this ingress class is not for us %s", ingressClass.Spec.Controller))
-		return ctrl.Result{}, nil
-	}
-
-	if ingressClass.Spec.Parameters == nil {
-		// without any params - this just a passthrough API?
-
-		return ctrl.Result{}, nil
-	}
-
-	if ingressClass.Spec.Parameters.Kind != "ApiDefinition" {
-		return ctrl.Result{}, fmt.Errorf("unknown Kind %s", ingressClass.Spec.Parameters.Kind)
-	}
-
-	if *ingressClass.Spec.Parameters.APIGroup != "tyk.tyk.io" {
-		return ctrl.Result{}, fmt.Errorf("unknown APIGroup %s", *ingressClass.Spec.Parameters.APIGroup)
+	// ensure there is a template api definition
+	templateAnnotationValue, ok := desired.Annotations[ingressTemplateAnnotationKey]
+	if !ok {
+		return ctrl.Result{}, fmt.Errorf("expecting template annotation %s", ingressTemplateAnnotationKey)
 	}
 
 	// we have parameters - as such, we should ensure that there is an api definition resource
 	template := &v1alpha1.ApiDefinition{}
-	err = r.Get(ctx, types.NamespacedName{Name: ingressClass.Spec.Parameters.Name, Namespace: req.Namespace}, template)
+	err := r.Get(ctx, types.NamespacedName{Name: templateAnnotationValue, Namespace: req.Namespace}, template)
 	if err != nil {
-		log.Error(err, "error getting api definition to use as a template", "name", ingressClass.Spec.Parameters.Name)
+		log.Error(err, "error getting api definition to use as a template", "name", templateAnnotationValue)
 		return ctrl.Result{}, err
 	}
 
-	ingressInNS := &v1beta1.IngressList{}
-	opts = []client.ListOption{
-		client.InNamespace(req.NamespacedName.Namespace),
-	}
-	if err := r.List(ctx, ingressInNS, opts...); err != nil {
-		return ctrl.Result{}, err
-	}
-	//for _, item := range ingressInNS.Items {
-	//	for _, rule := range item.Spec.Rules {
-	//		for _, path := range rule.HTTP.Paths {
-	//
-	//		}
-	//	}
-	//}
-
-	apiDefinitionsInNS := &v1alpha1.ApiDefinitionList{}
-	opts = []client.ListOption{
-		client.InNamespace(req.NamespacedName.Namespace),
-	}
-	if err := r.List(ctx, apiDefinitionsInNS, opts...); err != nil {
-		return ctrl.Result{}, err
-	}
-
-	//apiDefinitionsToDelete := &v1alpha1.ApiDefinitionList{}
-	apiDefinitionsToCreateOrUpdate := &v1alpha1.ApiDefinitionList{}
+	apisToCreateOrUpdate := v1alpha1.ApiDefinitionList{}
+	apisToUpdate := v1alpha1.ApiDefinitionList{}
+	apisToCreate := v1alpha1.ApiDefinitionList{}
+	apisToDelete := v1alpha1.ApiDefinitionList{}
 
 	for _, rule := range desired.Spec.Rules {
-		// TODO: precise match only for time being. Need to convert K8s * syntax to Tyk host regexp.
 		hostName := rule.Host
 
 		for _, p := range rule.HTTP.Paths {
-			api := template.DeepCopy()
-			api.Name = fmt.Sprintf("%s %s %s #%s", namespacedName.Namespace, namespacedName.Name, p.Path, ingressClass.Spec.Parameters.Name)
+			apiName := r.buildAPIName(req.Namespace, req.Name, hostName, p.Path)
+			api := v1alpha1.ApiDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      apiName,
+					Namespace: req.Namespace,
+				},
+				Spec: *template.Spec.DeepCopy(),
+			}
+			api.SetLabels(map[string]string{
+				labelKey: req.Name,
+			})
+
+			gvk := desired.GetObjectKind().GroupVersionKind()
+			api.SetOwnerReferences(append(api.GetOwnerReferences(), *metav1.NewControllerRef(desired, gvk)))
+
+			api.Spec.Name = apiName
 			api.Spec.Proxy.ListenPath = p.Path
 			api.Spec.Proxy.TargetURL = fmt.Sprintf("http://%s.%s.svc.cluster.local:%d", p.Backend.ServiceName, namespacedName.Namespace, p.Backend.ServicePort.IntValue())
+
 			api.Spec.Domain = hostName
 
-			apiDefinitionsToCreateOrUpdate.Items = append(apiDefinitionsToCreateOrUpdate.Items, *api)
+			if !strings.Contains(p.Path, ".well-known/acme-challenge") && !strings.Contains(p.Backend.ServiceName, "cm-acme-http-solver") {
+				for _, tls := range desired.Spec.TLS {
+					for _, host := range tls.Hosts {
+						if hostName == host {
+							api.Spec.Protocol = "https"
+							api.Spec.CertificateSecretNames = []string{
+								tls.SecretName,
+							}
+							api.Spec.ListenPort = 443
+						}
+					}
+				}
+			} else {
+				// for the acme challenge
+				api.Spec.Proxy.StripListenPath = false
+				api.Spec.Proxy.PreserveHostHeader = true
+			}
+
+			apisToCreateOrUpdate.Items = append(apisToCreateOrUpdate.Items, api)
+		}
+	}
+
+	// All the apis we should delete or update
+	for _, x := range oldAPIs.Items {
+		found := false
+		for _, y := range apisToCreateOrUpdate.Items {
+			if y.Name == x.Name {
+				apisToUpdate.Items = append(apisToUpdate.Items, y)
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			apisToDelete.Items = append(apisToDelete.Items, x)
+		}
+	}
+
+	// all the items to create
+	for _, x := range apisToCreateOrUpdate.Items {
+		create := true
+		for _, y := range apisToUpdate.Items {
+			if x.Name == y.Name {
+				create = false
+				break
+			}
+		}
+
+		if create {
+			apisToCreate.Items = append(apisToCreate.Items, x)
+		}
+	}
+
+	// create new endpoints first
+	for _, a := range apisToCreate.Items {
+		if err := r.Create(ctx, &a); err != nil {
+			log.Error(err, "unable to update api")
+		}
+	}
+
+	// update second
+	for _, a := range apisToUpdate.Items {
+		apiDefToUpdate := v1alpha1.ApiDefinition{}
+		err := r.Get(ctx, types.NamespacedName{Name: a.Name, Namespace: a.Namespace}, &apiDefToUpdate)
+		if err != nil {
+			log.Error(err, "unable to get api to update")
+			continue
+		}
+		apiDefToUpdate.Spec = a.Spec
+		if err := r.Update(ctx, &apiDefToUpdate); err != nil {
+			log.Error(err, "unable to update api")
+		}
+	}
+
+	// delete last - just in-case something renamed
+	for _, a := range apisToDelete.Items {
+		if err := r.Delete(ctx, &a); err != nil {
+			log.Error(err, "unable to update api")
 		}
 	}
 
 	return ctrl.Result{}, nil
 }
 
+func (r *IngressReconciler) buildAPIName(nameSpace, name, hostName, path string) string {
+	return fmt.Sprintf("%s-%s-%s", nameSpace, name, shortHash(hostName+path))
+}
+
+func shortHash(txt string) string {
+	h := sha256.New()
+	h.Write([]byte(txt))
+	return fmt.Sprintf("%x", h.Sum(nil))[:9]
+}
+
+func (r *IngressReconciler) ingressClassEventFilter() predicate.Predicate {
+	isOurIngress := func(annotations map[string]string) bool {
+		if ingressClass, ok := annotations[ingressClassAnnotationKey]; !ok {
+			return false
+		} else if ingressClass == defaultIngressClassAnnotationValue {
+			// if the ingress class is `tyk` it's for us
+			return true
+		}
+		// TODO: env var?
+
+		return false
+	}
+
+	return predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			return isOurIngress(e.Meta.GetAnnotations())
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			return isOurIngress(e.MetaNew.GetAnnotations())
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			return isOurIngress(e.Meta.GetAnnotations())
+		},
+	}
+}
+
 func (r *IngressReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	//err := mgr.GetFieldIndexer().
+	//	IndexField(context.TODO(), &v1alpha1.ApiDefinition{}, apiOwnerKey, func(rawObj runtime.Object) []string {
+	//		// grab the apiDef object, extract the owner...
+	//		apiDefinition := rawObj.(*v1alpha1.ApiDefinition)
+	//		owner := metav1.GetControllerOf(apiDefinition)
+	//		if owner == nil {
+	//			return nil
+	//		}
+	//
+	//		if owner.APIVersion != ingressGVString || owner.Kind != "Ingress" {
+	//			return nil
+	//		}
+	//
+	//		return []string{owner.Name}
+	//	})
+
+	//if err != nil {
+	//	return err
+	//}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1beta1.Ingress{}).
+		Owns(&v1alpha1.ApiDefinition{}).
+		WithEventFilter(r.ingressClassEventFilter()).
 		Complete(r)
 }
