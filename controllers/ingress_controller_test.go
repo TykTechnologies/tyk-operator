6a85f6732947b3749187c7a4ea37c03fb42ef250
gernest <geofreyernest@live.com>
2020-12-29 17:11:47 +0300

    update TestShortHash test

--- controllers/ingress_controller_test.go
+++ controllers/ingress_controller_test.go
@@ -3,7 +3,7 @@ package controllers
 import "testing"
 
 func TestShortHash(t *testing.T) {
-	if expect, got := "c09b5928e", shortHash("foo.com"+"/httpbin22222"); got != expect {
+	if expect, got := "c09b", shortHash("foo.com"+"/httpbin22222"); got != expect {
 		t.Errorf("expected %v got %v", expect, got)
 	}
 }
