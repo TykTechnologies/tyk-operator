c6cf5ddbcc949f9c8430ef41bd3767e25c205c52
Geofrey Ernest <geofreyernest@live.com>
2020-12-29 22:01:18 +0300

    Use controllerutil in security policy reconciler (#215)
    * add ErrNotFound and helper methods
    
    This is returned when we get 404 from api calls.
    
    * fix reconciler
    
    * simplify mutatefn

--- pkg/universal_client/client.go
+++ pkg/universal_client/client.go
@@ -16,8 +16,24 @@ import (
 // ErrTODO is returned when a feature is not yet implemented
 var ErrTODO = errors.New("TODO: This feature is not implemented yet")
 
+// ErrNotFound is returned when an api call returns 404
+var ErrNotFound = errors.New("Resource not found")
+
 func IsTODO(err error) bool {
 	return errors.Is(err, ErrTODO)
+}
+
+// IsNotFound returns true if err is ErrNotFound
+func IsNotFound(err error) bool {
+	return errors.Is(err, ErrNotFound)
+}
+
+// IgnoreNotFound returns nil if err is ErrNotFound
+func IgnoreNotFound(err error) error {
+	if !IsNotFound(err) {
+		return err
+	}
+	return nil
 }
 
 var client = &http.Client{}
@@ -103,6 +119,10 @@ func (c Client) Call(method, url string, body io.Reader, fn ...func(*http.Reques
 	}
 	values = append(values)
 	c.Log.Info("Call", values...)
+	if err == nil && res.StatusCode == http.StatusNotFound {
+		res.Body.Close()
+		return nil, ErrNotFound
+	}
 	return res, err
 }
 
