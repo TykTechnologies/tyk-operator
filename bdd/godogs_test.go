8e665d815450c2f1cbd3ecbc7ecc2d2158eea430
gernest <geofreyernest@live.com>
2021-01-04 14:14:30 +0300

    revert delay

--- bdd/godogs_test.go
+++ bdd/godogs_test.go
@@ -24,7 +24,7 @@ import (
 const (
 	namespace      = "bdd"
 	k8sTimeout     = time.Second * 10
-	reconcileDelay = time.Second * 10
+	reconcileDelay = time.Second * 5
 )
 
 var gwNS = fmt.Sprintf("tyk%s-control-plane", os.Getenv("TYK_MODE"))
