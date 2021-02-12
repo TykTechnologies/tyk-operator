7046458cef4d2ceece5a337638b07b2ce115b88e
Geofrey Ernest <geofreyernest@live.com>
2021-02-12 13:11:57 +0300

    Cleanup JWT support (#248)
    * validate jwt signing methods
    
    * remove JWTSigningMethod type
    
    * Change how we link api definition to policies
    
    This adds LinkedPolicies in `ApiDefinitionStatus` . This tracks a list
    of policies linking to the api definition.
    
    When the `ApiDefinition` resource is deleted, we make sure that no policy that
    is linked is still active in the cluster before approving the deletion.
    
    On the SecurityPolicy controller we keep the linked ApiDefinition up to date
    with SecurityPolicy resource.
    
    * fix checking linked policies
    
    * fix updating linked policies
    
    * cleanup
    
    * check linked api's  before policy deletion
    
    * cleanup and proper track linked policies
    
    * cleanup
    
    * cleanup
    
    * fix docs
    
    * update crd with the docs fix
    
    * add documentation
    
    * updatejwt docs
    
    * add comment for JWTClientIDBaseField
    
    * generate mannifests
    
    * remove debug stuff
    
    * support method transform middleware (#240)
    
    * add bdd test for method transform
    
    * add newline
    
    * cleanup bdd transform resource
    
    * remove url_rewrite and use /anything
    
    * update feature support matrix
    
    * update api_definitions.md
    
    * update api_definitions.md
    
    * Update api_definitions.md
    
    * add session_lifetime (#255)
    
    * ecdsa example
    
    * default policy and explicit policy examples
    
    * linking to examples
    
    Co-authored-by: Ahmet Soormally <ahmet@mangomm.co.uk>

--- controllers/apidefinition_controller.go
+++ controllers/apidefinition_controller.go
@@ -18,8 +18,10 @@ package controllers
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/TykTechnologies/tyk-operator/pkg/cert"
@@ -37,8 +39,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	util "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
+
+const queueAfter = time.Second * 5
 
 // ApiDefinitionReconciler reconciles a ApiDefinition object
 type ApiDefinitionReconciler struct {
@@ -58,7 +61,7 @@ func (r *ApiDefinitionReconciler) Reconcile(req ctrl.Request) (ctrl.Result, erro
 	namespacedName := req.NamespacedName
 
 	log := r.Log.WithValues("ApiDefinition", namespacedName.String())
-
+	log.Info("Reconciling ApiDefinition instance")
 	desired := &tykv1alpha1.ApiDefinition{}
 	if err := r.Get(ctx, req.NamespacedName, desired); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err) // Ignore not-found errors
@@ -75,128 +78,83 @@ func (r *ApiDefinitionReconciler) Reconcile(req ctrl.Request) (ctrl.Result, erro
 		return ctrl.Result{}, nil
 	}
 
-	// If object is being deleted
-	if !desired.ObjectMeta.DeletionTimestamp.IsZero() {
-		log.Info("resource being deleted")
-		// If our finalizer is present, need to delete from Tyk still
-		if util.ContainsFinalizer(desired, keys.ApiDefFinalizerName) {
-			log.Info("checking linked security policies")
-			policies, err := r.UniversalClient.SecurityPolicy().All()
-			if err != nil && !universal_client.IsTODO(err) {
-				log.Info(err.Error())
-				return ctrl.Result{RequeueAfter: time.Second * 5}, err
+	var queue bool
+	var queueA time.Duration
+	_, err := util.CreateOrUpdate(ctx, r.Client, desired, func() error {
+		if !desired.ObjectMeta.DeletionTimestamp.IsZero() {
+			e, err := r.delete(ctx, desired)
+			queueA = e
+			return err
+		}
+		if desired.Spec.APIID == "" {
+			desired.Spec.APIID = encodeNS(req.NamespacedName.String())
+		}
+		util.AddFinalizer(desired, keys.ApiDefFinalizerName)
+		for _, certName := range desired.Spec.CertificateSecretNames {
+			secret := v1.Secret{}
+			err := r.Get(ctx, types.NamespacedName{Name: certName, Namespace: namespacedName.Namespace}, &secret)
+			if err != nil {
+				log.Error(err, "requeueing because secret not found")
+				return err
+			}
+			pemCrtBytes, ok := secret.Data["tls.crt"]
+			if !ok {
+				log.Error(err, "requeueing because cert not found in secret")
+				return err
 			}
 
-			for _, policy := range policies {
-				for _, right := range policy.AccessRightsArray {
-					if right.APIID == desired.Status.ApiID {
-						log.Info("unable to delete api due to security policy dependency",
-							"api", namespacedName.String(),
-							"policy", apiIDDecode(policy.ID),
-						)
-						return ctrl.Result{RequeueAfter: time.Second * 5}, err
-					}
+			pemKeyBytes, ok := secret.Data["tls.key"]
+			if !ok {
+				log.Error(err, "requeueing because key not found in secret")
+				return err
+			}
+
+			tykCertID := r.UniversalClient.Organization().GetID() + cert.CalculateFingerPrint(pemCrtBytes)
+			exists := r.UniversalClient.Certificate().Exists(tykCertID)
+			if !exists {
+				// upload the certificate
+				tykCertID, err = r.UniversalClient.Certificate().Upload(pemKeyBytes, pemCrtBytes)
+				if err != nil {
+					queue = true
+					return err
 				}
 			}
-
-			log.Info("deleting api")
-			err = r.UniversalClient.Api().Delete(desired.Status.ApiID)
+			desired.Spec.Certificates = []string{tykCertID}
+			break
+		}
+		desired.Spec.CertificateSecretNames = nil
+		r.updateLinkedPolicies(ctx, desired)
+		//  If this is not set, means it is a new object, set it first
+		if desired.Status.ApiID == "" {
+			err := r.UniversalClient.Api().Create(&desired.Spec)
 			if err != nil {
-				log.Error(err, "unable to delete api", "api_id", desired.Status.ApiID)
-				return ctrl.Result{}, err
+				log.Error(err, "Failed to create api definition")
+				return err
 			}
-
-			err = r.UniversalClient.HotReload()
+			desired.Status.ApiID = desired.Spec.APIID
+			err = r.Status().Update(ctx, desired)
 			if err != nil {
-				log.Error(err, "unable to hot reload", "api_id", desired.Status.ApiID)
-				return ctrl.Result{}, err
+				log.Error(err, "Could not update Status ID")
 			}
-
-			log.Info("removing finalizer")
-			util.RemoveFinalizer(desired, keys.ApiDefFinalizerName)
-			if err := r.Update(ctx, desired); err != nil {
-				return reconcile.Result{}, err
-			}
+			r.UniversalClient.HotReload()
+			return client.IgnoreNotFound(err)
 		}
-		log.Info("done")
-		return reconcile.Result{}, nil
-	}
-	if desired.Spec.APIID == "" {
-		desired.Spec.APIID = encodeNS(req.NamespacedName.String())
-	}
-	if !util.ContainsFinalizer(desired, keys.ApiDefFinalizerName) {
-		log.Info("adding finalizer")
-		desired.ObjectMeta.Finalizers = append(desired.ObjectMeta.Finalizers, keys.ApiDefFinalizerName)
-		err := r.Update(ctx, desired)
-		// Return either way because the update will
-		// issue a requeue anyway
-		return reconcile.Result{}, client.IgnoreNotFound(err)
-	}
-
-	for _, certName := range desired.Spec.CertificateSecretNames {
-		secret := v1.Secret{}
-		err := r.Get(ctx, types.NamespacedName{Name: certName, Namespace: namespacedName.Namespace}, &secret)
+		log.Info("Updating ApiDefinition")
+		desired.Spec.APIID = desired.Status.ApiID
+		err := r.UniversalClient.Api().Update(&desired.Spec)
 		if err != nil {
-			log.Error(err, "requeueing because secret not found")
-			return reconcile.Result{}, err
-		}
-
-		pemCrtBytes, ok := secret.Data["tls.crt"]
-		if !ok {
-			log.Error(err, "requeueing because cert not found in secret")
-			return reconcile.Result{}, err
-		}
-
-		pemKeyBytes, ok := secret.Data["tls.key"]
-		if !ok {
-			log.Error(err, "requeueing because key not found in secret")
-			return reconcile.Result{}, err
-		}
-
-		tykCertID := r.UniversalClient.Organization().GetID() + cert.CalculateFingerPrint(pemCrtBytes)
-		exists := r.UniversalClient.Certificate().Exists(tykCertID)
-		if !exists {
-			// upload the certificate
-			tykCertID, err = r.UniversalClient.Certificate().Upload(pemKeyBytes, pemCrtBytes)
 			if err != nil {
-				return reconcile.Result{Requeue: true}, err
+				log.Error(err, "Failed to update api definition")
+				return err
 			}
-		}
-
-		desired.Spec.Certificates = []string{tykCertID}
-		break
-	}
-
-	desired.Spec.CertificateSecretNames = nil
-
-	//  If this is not set, means it is a new object, set it first
-	if desired.Status.ApiID == "" {
-		err := r.UniversalClient.Api().Create(&desired.Spec)
-		if err != nil {
-			log.Error(err, "Failed to create api definition")
-			return ctrl.Result{}, err
-		}
-		desired.Status.ApiID = desired.Spec.APIID
-		err = r.Status().Update(ctx, desired)
-		if err != nil {
-			log.Error(err, "Could not update Status ID")
 		}
 		r.UniversalClient.HotReload()
-		return ctrl.Result{}, client.IgnoreNotFound(err)
+		return nil
+	})
+	if err == nil {
+		log.Info("Completed reconciling ApiDefinition instance")
 	}
-
-	log.Info("Updating ApiDefinition")
-	desired.Spec.APIID = desired.Status.ApiID
-	err := r.UniversalClient.Api().Update(&desired.Spec)
-	if err != nil {
-		if err != nil {
-			log.Error(err, "Failed to update api definition")
-			return ctrl.Result{}, err
-		}
-	}
-	r.UniversalClient.HotReload()
-	log.Info("done")
-	return ctrl.Result{}, nil
+	return ctrl.Result{Requeue: queue, RequeueAfter: queueA}, err
 }
 
 // This triggers an update to all ingress resources that have template
@@ -204,7 +162,6 @@ func (r *ApiDefinitionReconciler) Reconcile(req ctrl.Request) (ctrl.Result, erro
 //
 // We return nil when a is being deleted and do nothing.
 func (r *ApiDefinitionReconciler) syncTemplate(ctx context.Context, ns string, a *tykv1alpha1.ApiDefinition) (ctrl.Result, error) {
-
 	if !a.DeletionTimestamp.IsZero() {
 		if util.ContainsFinalizer(a, keys.ApiDefTemplateFinalizerName) {
 			ls := v1beta1.IngressList{}
@@ -261,6 +218,75 @@ func (r *ApiDefinitionReconciler) syncTemplate(ctx context.Context, ns string, a
 	return ctrl.Result{}, nil
 }
 
+func (r *ApiDefinitionReconciler) delete(ctx context.Context, desired *tykv1alpha1.ApiDefinition) (time.Duration, error) {
+	r.Log.Info("resource being deleted")
+	// If our finalizer is present, need to delete from Tyk still
+	if util.ContainsFinalizer(desired, keys.ApiDefFinalizerName) {
+		if err := r.checkLinkedPolicies(ctx, desired); err != nil {
+			return queueAfter, err
+		}
+		r.Log.Info("deleting api")
+		err := r.UniversalClient.Api().Delete(desired.Status.ApiID)
+		if err != nil {
+			r.Log.Error(err, "unable to delete api", "api_id", desired.Status.ApiID)
+			return 0, err
+		}
+		err = r.UniversalClient.HotReload()
+		if err != nil {
+			r.Log.Error(err, "unable to hot reload", "api_id", desired.Status.ApiID)
+			return 0, err
+		}
+		r.Log.Info("removing finalizer")
+		util.RemoveFinalizer(desired, keys.ApiDefFinalizerName)
+	}
+	return 0, nil
+}
+
+// checkLinkedPolicies checks if there are any policies that are still linking
+// to this api defintion resource.
+func (r *ApiDefinitionReconciler) checkLinkedPolicies(ctx context.Context, a *tykv1alpha1.ApiDefinition) error {
+	r.Log.Info("checking linked security policies")
+	if len(a.Status.LinkedByPolicies) == 0 {
+		return nil
+	}
+	for _, n := range a.Status.LinkedByPolicies {
+		p := strings.Split(n, string(types.Separator))
+		if len(p) != 2 {
+			err := fmt.Errorf("malformed linked_policy expected namespace/name format got %s", n)
+			r.Log.Error(err, "Failed to parse lined_policies")
+			return err
+		}
+		ns := types.NamespacedName{Namespace: p[0], Name: p[1]}
+		var policy tykv1alpha1.SecurityPolicy
+		if err := r.Get(ctx, ns, &policy); err == nil {
+			return fmt.Errorf("unable to delete api due to security policy dependency=%s", n)
+		}
+	}
+	return nil
+}
+
+func encodeIfNotBase64(s string) string {
+	_, err := base64.URLEncoding.DecodeString(s)
+	if err == nil {
+		return s
+	}
+	return encodeNS(s)
+}
+
+// updateLinkedPolicies ensure that all policies needed by this api denition are
+// updated.
+func (r *ApiDefinitionReconciler) updateLinkedPolicies(ctx context.Context, a *tykv1alpha1.ApiDefinition) {
+	r.Log.Info("Updating linked policies")
+	for x := range a.Spec.JWTDefaultPolicies {
+		a.Spec.JWTDefaultPolicies[x] = encodeIfNotBase64(a.Spec.JWTDefaultPolicies[x])
+	}
+	for k, x := range a.Spec.JWTScopeToPolicyMapping {
+		a.Spec.JWTScopeToPolicyMapping[k] = encodeIfNotBase64(x)
+	}
+	return
+}
+
+// SetupWithManager initializes the api definition controller.
 func (r *ApiDefinitionReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&tykv1alpha1.ApiDefinition{}).
--- controllers/securitypolicy_controller.go
+++ controllers/securitypolicy_controller.go
@@ -19,6 +19,7 @@ package controllers
 import (
 	"context"
 	"os"
+	"time"
 
 	tykv1 "github.com/TykTechnologies/tyk-operator/api/v1alpha1"
 	"github.com/TykTechnologies/tyk-operator/pkg/environmet"
@@ -60,8 +61,8 @@ func (r *SecurityPolicyReconciler) Reconcile(req ctrl.Request) (ctrl.Result, err
 	if err := r.Get(ctx, req.NamespacedName, policy); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-
-	op, err := util.CreateOrUpdate(ctx, r.Client, policy, func() error {
+	var reqA time.Duration
+	_, err := util.CreateOrUpdate(ctx, r.Client, policy, func() error {
 		if !policy.ObjectMeta.DeletionTimestamp.IsZero() {
 			if util.ContainsFinalizer(policy, policyFinalizer) {
 				return r.delete(ctx, policy)
@@ -79,17 +80,11 @@ func (r *SecurityPolicyReconciler) Reconcile(req ctrl.Request) (ctrl.Result, err
 		}
 		// update access rights
 		r.Log.Info("updating access rights")
+		var err error
 		for i := 0; i < len(policy.Spec.AccessRightsArray); i++ {
 			a := &policy.Spec.AccessRightsArray[i]
-			err := r.updateAccess(ctx, a)
+			reqA, err = r.updateAccess(ctx, a, ns)
 			if err != nil {
-				if errors.IsNotFound(err) {
-					r.Log.Info("APIDefinition resource was not found",
-						"Name", a.Name,
-						"Namespace", a.Namespace,
-					)
-					return nil
-				}
 				return err
 			}
 		}
@@ -98,30 +93,36 @@ func (r *SecurityPolicyReconciler) Reconcile(req ctrl.Request) (ctrl.Result, err
 		}
 		return r.update(ctx, policy)
 	})
-	if err != nil {
-		if errors.IsNotFound(err) {
-			return ctrl.Result{}, nil
-		}
-		r.Log.Error(err, "Failed create or update security policy ", "Op", op)
-		return ctrl.Result{}, err
+	if err == nil {
+		r.Log.Info("Completed reconciling SecurityPolicy instance")
 	}
-	r.Log.Info("Done reconcile", "Op", op)
-	return ctrl.Result{}, nil
+	return ctrl.Result{RequeueAfter: reqA}, err
 }
 
 func (r *SecurityPolicyReconciler) updateAccess(ctx context.Context,
-	a *tykv1.AccessDefinition) error {
+	a *tykv1.AccessDefinition, namespacedName string) (time.Duration, error) {
 	api := &tykv1.ApiDefinition{}
 	if err := r.Get(ctx, types.NamespacedName{Name: a.Name, Namespace: a.Namespace}, api); err != nil {
-		return err
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			r.Log.Info("ApiDefinition resource not found. Unable to attach to SecurityPolicy. ReQueue",
+				"Name", a.Name,
+				"Namespace", a.Namespace,
+			)
+			return queueAfter, err
+		}
+		r.Log.Error(err, "Failed to get APIDefinition to attach to SecurityPolicy")
+		return queueAfter, err
 	}
 	def, err := r.UniversalClient.Api().Get(api.Status.ApiID)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	a.APIID = def.APIID
 	a.APIName = def.Name
-	return nil
+	return 0, nil
 }
 
 func (r *SecurityPolicyReconciler) delete(ctx context.Context, policy *tykv1.SecurityPolicy) error {
@@ -133,6 +134,12 @@ func (r *SecurityPolicyReconciler) delete(ctx context.Context, policy *tykv1.Sec
 			return nil
 		}
 		r.Log.Error(err, "Failed to delete resource")
+		return err
+	}
+	err := r.updateLinkedAPI(ctx, policy, func(ads *tykv1.ApiDefinitionStatus, ns string) {
+		ads.LinkedByPolicies = removeString(ads.LinkedByPolicies, ns)
+	})
+	if err != nil {
 		return err
 	}
 	r.Log.Info("Successfully deleted Policy")
@@ -147,6 +154,12 @@ func (r *SecurityPolicyReconciler) update(ctx context.Context, policy *tykv1.Sec
 		r.Log.Error(err, "Failed to update policy")
 		return err
 	}
+	err = r.updateLinkedAPI(ctx, policy, func(ads *tykv1.ApiDefinitionStatus, s string) {
+		ads.LinkedByPolicies = addString(ads.LinkedByPolicies, s)
+	})
+	if err != nil {
+		return err
+	}
 	r.UniversalClient.HotReload()
 	r.Log.Info("Successfully updated Policy")
 	return nil
@@ -159,11 +172,37 @@ func (r *SecurityPolicyReconciler) create(ctx context.Context, policy *tykv1.Sec
 		r.Log.Error(err, "Failed to create policy")
 		return err
 	}
+	err = r.updateLinkedAPI(ctx, policy, func(ads *tykv1.ApiDefinitionStatus, s string) {
+		ads.LinkedByPolicies = addString(ads.LinkedByPolicies, s)
+	})
 	r.Log.Info("Successful created Policy")
 	policy.Status.PolID = policy.Spec.MID
 	return r.Status().Update(ctx, policy)
 }
 
+// updateLinkedAPI updates the status of api definitions associated with this
+// policy.
+func (r *SecurityPolicyReconciler) updateLinkedAPI(ctx context.Context, policy *tykv1.SecurityPolicy,
+	fn func(*tykv1.ApiDefinitionStatus, string),
+) error {
+	r.Log.Info("Updating linked api definitions")
+	ns := (types.NamespacedName{Namespace: policy.Namespace, Name: policy.Name}).String()
+	for _, a := range policy.Spec.AccessRightsArray {
+		api := &tykv1.ApiDefinition{}
+		if err := r.Get(ctx, types.NamespacedName{Name: a.Name, Namespace: a.Namespace}, api); err != nil {
+			r.Log.Error(err, "Failed to get linked api definition")
+			return err
+		}
+		fn(&api.Status, ns)
+		if err := r.Status().Update(ctx, api); err != nil {
+			r.Log.Error(err, "Failed to update linked api definition")
+			return err
+		}
+	}
+	return nil
+}
+
+// SetupWithManager initializes the security policy controller.
 func (r *SecurityPolicyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&tykv1.SecurityPolicy{}).
