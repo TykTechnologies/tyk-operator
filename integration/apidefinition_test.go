8781d2ab5be47a8652d6ec892615ca5b3ff9dfda
andrei-tyk <97896463+andrei-tyk@users.noreply.github.com>
2022-02-18 10:53:36 +0200

    [TT-3699] Enable white/black/ingored lists for endpoint designer via operator (#390)
    * enabled white/black/ignored endpoint designer options
    
    Signed-off-by: Andrei <andrei@tyk.io>
    
    * updated api_definition.md file
    
    Signed-off-by: Andrei <andrei@tyk.io>
    
    * updated examples
    
    Signed-off-by: Andrei <andrei@tyk.io>
    
    * added tests using the e2e framework for white/black/ignore lists
    
    * fixed linter feedback
    
    * fixed gofumpt issues
    
    * implemented code review feedback for whitelist/blacklist/ignored
    
    * reverted testing timeout to 30 mins
    
    * implemented CR feedback:
    - changed the names of the ApiDefinitions and Spec to better reflect the usecase
    - merged master
    
    * go fmted test file

--- integration/apidefinition_test.go
+++ integration/apidefinition_test.go
@@ -127,3 +127,384 @@ func TestApiDefinitionCreate(t *testing.T) {
 
 	testenv.Test(t, adCreate)
 }
+
+func TestApiDefinitionCreateWhitelist(t *testing.T) {
+	var (
+		apiDefWithWhitelist      = "apidef-whitelist"
+		apiDefListenPath         = "/test"
+		defaultVersion           = "Default"
+		errForbiddenResponseCode = 403
+		defaultTimeout           = 1 * time.Minute
+	)
+
+	const whiteListedPath = "/whitelisted"
+	eps := &model.ExtendedPathsSet{
+		WhiteList: []model.EndPointMeta{{
+			Path:       whiteListedPath,
+			IgnoreCase: true,
+			MethodActions: map[string]model.EndpointMethodMeta{
+				"GET": {
+					Action: "no_action",
+					Code:   200, Data: "",
+					Headers: make(map[string]string),
+				},
+			},
+		}},
+	}
+
+	adCreate := features.New("ApiDefinition").
+		Setup(func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+			testNS := ctx.Value(ctxNSKey).(string) //nolint:errcheck
+			is := is.New(t)
+
+			// Create ApiDefinition with whitelist extended path
+			_, err := createTestAPIDef(ctx, testNS, func(apiDef *v1alpha1.ApiDefinition) {
+				apiDef.Name = apiDefWithWhitelist
+				apiDef.Spec.Proxy = model.Proxy{
+					ListenPath:      apiDefListenPath,
+					TargetURL:       "http://httpbin.org",
+					StripListenPath: true,
+				}
+				apiDef.Spec.VersionData.DefaultVersion = defaultVersion
+				apiDef.Spec.VersionData.NotVersioned = true
+				apiDef.Spec.VersionData.Versions = map[string]model.VersionInfo{
+					defaultVersion: {Name: defaultVersion, UseExtendedPaths: true, ExtendedPaths: eps},
+				}
+			}, envConf)
+			is.NoErr(err) // failed to create apiDefinition
+
+			return ctx
+		}).
+		Assess("ApiDefinition must have whitelist field",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				is := is.New(t)
+				client := cfg.Client()
+
+				testNS := ctx.Value(ctxNSKey).(string) //nolint:errcheck
+				desiredApiDef := v1alpha1.ApiDefinition{
+					ObjectMeta: metav1.ObjectMeta{Name: apiDefWithWhitelist, Namespace: testNS},
+				}
+
+				err := wait.For(conditions.New(client.Resources()).ResourceMatch(&desiredApiDef, func(object k8s.Object) bool {
+					apiDef := object.(*v1alpha1.ApiDefinition) //nolint:errcheck
+					return len(apiDef.Spec.VersionData.Versions[defaultVersion].ExtendedPaths.WhiteList) == 1
+				}), wait.WithTimeout(defaultTimeout))
+				is.NoErr(err)
+
+				return ctx
+			}).
+		Assess("ApiDefniition should allow traffic to whitelisted route",
+			func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+				is := is.New(t)
+
+				err := wait.For(func() (done bool, err error) {
+					hc := &http.Client{}
+
+					req, err := http.NewRequest(
+						http.MethodGet,
+						fmt.Sprintf("%s%s", gatewayLocalhost, apiDefListenPath+whiteListedPath),
+						nil,
+					)
+					is.NoErr(err)
+
+					resp, err := hc.Do(req)
+					is.NoErr(err)
+
+					if resp.StatusCode == errForbiddenResponseCode {
+						return false, nil
+					}
+
+					return true, nil
+				}, wait.WithTimeout(defaultTimeout))
+				is.NoErr(err)
+				return ctx
+			}).
+		Assess("ApiDefinition must not allow traffic to non-whitelisted route",
+			func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+				is := is.New(t)
+
+				err := wait.For(func() (done bool, err error) {
+					hc := &http.Client{}
+
+					req, err := http.NewRequest(
+						http.MethodGet,
+						fmt.Sprintf("%s%s", gatewayLocalhost, apiDefListenPath),
+						nil,
+					)
+					is.NoErr(err)
+					req.Header.Add("Content-type", "application/json")
+
+					resp, err := hc.Do(req)
+					is.NoErr(err)
+
+					if resp.StatusCode != errForbiddenResponseCode {
+						return false, nil
+					}
+
+					return true, nil
+				}, wait.WithTimeout(defaultTimeout))
+				is.NoErr(err)
+
+				return ctx
+			}).Feature()
+
+	testenv.Test(t, adCreate)
+}
+
+func TestApiDefinitionCreateBlackList(t *testing.T) {
+	var (
+		apiDefWithBlacklist      = "apidef-blacklist"
+		apiDefListenPath         = "/test"
+		defaultVersion           = "Default"
+		errForbiddenResponseCode = 403
+		defaultTimeout           = 1 * time.Minute
+	)
+
+	const blackListedPath = "/blacklisted"
+	eps := &model.ExtendedPathsSet{
+		BlackList: []model.EndPointMeta{{
+			Path:       blackListedPath,
+			IgnoreCase: true,
+			MethodActions: map[string]model.EndpointMethodMeta{
+				"GET": {
+					Action: "no_action",
+					Code:   200, Data: "",
+					Headers: make(map[string]string),
+				},
+			},
+		}},
+	}
+
+	adCreate := features.New("ApiDefinition").
+		Setup(func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+			testNS := ctx.Value(ctxNSKey).(string) //nolint:errcheck
+			is := is.New(t)
+
+			// Create ApiDefinition with whitelist extended path
+			_, err := createTestAPIDef(ctx, testNS, func(apiDef *v1alpha1.ApiDefinition) {
+				apiDef.Name = apiDefWithBlacklist
+				apiDef.Spec.Proxy = model.Proxy{
+					ListenPath:      apiDefListenPath,
+					TargetURL:       "http://httpbin.org",
+					StripListenPath: true,
+				}
+				apiDef.Spec.VersionData.DefaultVersion = defaultVersion
+				apiDef.Spec.VersionData.NotVersioned = true
+				apiDef.Spec.VersionData.Versions = map[string]model.VersionInfo{
+					defaultVersion: {Name: defaultVersion, UseExtendedPaths: true, ExtendedPaths: eps},
+				}
+			}, envConf)
+			is.NoErr(err) // failed to create apiDefinition
+
+			return ctx
+		}).
+		Assess("ApiDefinition must have blacklist field",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				is := is.New(t)
+				client := cfg.Client()
+
+				testNS := ctx.Value(ctxNSKey).(string) //nolint:errcheck
+				desiredApiDef := v1alpha1.ApiDefinition{
+					ObjectMeta: metav1.ObjectMeta{Name: apiDefWithBlacklist, Namespace: testNS},
+				}
+
+				err := wait.For(conditions.New(client.Resources()).ResourceMatch(&desiredApiDef, func(object k8s.Object) bool {
+					apiDef := object.(*v1alpha1.ApiDefinition) //nolint:errcheck
+					return len(apiDef.Spec.VersionData.Versions[defaultVersion].ExtendedPaths.BlackList) == 1
+				}), wait.WithTimeout(defaultTimeout))
+				is.NoErr(err)
+
+				return ctx
+			}).
+		Assess("ApiDefniition should forbid traffic to blacklist route",
+			func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+				is := is.New(t)
+
+				err := wait.For(func() (done bool, err error) {
+					hc := &http.Client{}
+
+					req, err := http.NewRequest(
+						http.MethodGet,
+						fmt.Sprintf("%s%s", gatewayLocalhost, apiDefListenPath+blackListedPath),
+						nil,
+					)
+					is.NoErr(err)
+
+					resp, err := hc.Do(req)
+					is.NoErr(err)
+
+					if resp.StatusCode != errForbiddenResponseCode {
+						return false, nil
+					}
+
+					return true, nil
+				}, wait.WithTimeout(defaultTimeout))
+				is.NoErr(err)
+				return ctx
+			}).
+		Assess("ApiDefinition must allow traffic to non-blacklisted route",
+			func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+				is := is.New(t)
+
+				err := wait.For(func() (done bool, err error) {
+					hc := &http.Client{}
+
+					req, err := http.NewRequest(
+						http.MethodGet,
+						fmt.Sprintf("%s%s", gatewayLocalhost, apiDefListenPath),
+						nil,
+					)
+					is.NoErr(err)
+					req.Header.Add("Content-type", "application/json")
+
+					resp, err := hc.Do(req)
+					is.NoErr(err)
+
+					if resp.StatusCode == errForbiddenResponseCode {
+						return false, nil
+					}
+
+					return true, nil
+				}, wait.WithTimeout(defaultTimeout))
+				is.NoErr(err)
+
+				return ctx
+			}).Feature()
+
+	testenv.Test(t, adCreate)
+}
+
+func TestApiDefinitionCreateIgnored(t *testing.T) {
+	var (
+		apiDefWithWhitelist      = "apidef-ignored"
+		apiDefListenPath         = "/test"
+		defaultVersion           = "Default"
+		errForbiddenResponseCode = 403
+		defaultTimeout           = 1 * time.Minute
+	)
+
+	const whiteListedPath = "/whitelisted"
+	const ignoredPath = "/ignored"
+	eps := &model.ExtendedPathsSet{
+		WhiteList: []model.EndPointMeta{{
+			Path:       whiteListedPath,
+			IgnoreCase: true,
+			MethodActions: map[string]model.EndpointMethodMeta{
+				"GET": {
+					Action: "no_action",
+					Code:   200, Data: "",
+					Headers: make(map[string]string),
+				},
+			},
+		}},
+		Ignored: []model.EndPointMeta{{
+			Path:       ignoredPath,
+			IgnoreCase: true,
+			MethodActions: map[string]model.EndpointMethodMeta{
+				"GET": {
+					Action: "no_action",
+					Code:   200, Data: "",
+					Headers: make(map[string]string),
+				},
+			},
+		}},
+	}
+
+	adCreate := features.New("ApiDefinition").
+		Setup(func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+			testNS := ctx.Value(ctxNSKey).(string) //nolint:errcheck
+			is := is.New(t)
+
+			// Create ApiDefinition with whitelist + ingored extended path
+			_, err := createTestAPIDef(ctx, testNS, func(apiDef *v1alpha1.ApiDefinition) {
+				apiDef.Name = apiDefWithWhitelist
+				apiDef.Spec.Proxy = model.Proxy{
+					ListenPath:      apiDefListenPath,
+					TargetURL:       "http://httpbin.org",
+					StripListenPath: true,
+				}
+				apiDef.Spec.VersionData.DefaultVersion = defaultVersion
+				apiDef.Spec.VersionData.NotVersioned = true
+				apiDef.Spec.VersionData.Versions = map[string]model.VersionInfo{
+					defaultVersion: {Name: defaultVersion, UseExtendedPaths: true, ExtendedPaths: eps},
+				}
+			}, envConf)
+			is.NoErr(err) // failed to create apiDefinition
+
+			return ctx
+		}).
+		Assess("ApiDefinition must have ignored field",
+			func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				is := is.New(t)
+				client := cfg.Client()
+
+				testNS := ctx.Value(ctxNSKey).(string) //nolint:errcheck
+				desiredApiDef := v1alpha1.ApiDefinition{
+					ObjectMeta: metav1.ObjectMeta{Name: apiDefWithWhitelist, Namespace: testNS},
+				}
+
+				err := wait.For(conditions.New(client.Resources()).ResourceMatch(&desiredApiDef, func(object k8s.Object) bool {
+					apiDef := object.(*v1alpha1.ApiDefinition) //nolint:errcheck
+					return len(apiDef.Spec.VersionData.Versions[defaultVersion].ExtendedPaths.Ignored) == 1
+				}), wait.WithTimeout(defaultTimeout))
+				is.NoErr(err)
+
+				return ctx
+			}).
+		Assess("ApiDefniition should allow traffic to ignored route",
+			func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+				is := is.New(t)
+
+				err := wait.For(func() (done bool, err error) {
+					hc := &http.Client{}
+
+					req, err := http.NewRequest(
+						http.MethodGet,
+						fmt.Sprintf("%s%s", gatewayLocalhost, apiDefListenPath+ignoredPath),
+						nil,
+					)
+					is.NoErr(err)
+
+					resp, err := hc.Do(req)
+					is.NoErr(err)
+
+					if resp.StatusCode == errForbiddenResponseCode {
+						return false, nil
+					}
+
+					return true, nil
+				}, wait.WithTimeout(defaultTimeout))
+				is.NoErr(err)
+				return ctx
+			}).
+		Assess("ApiDefinition must not allow traffic to other non whitelisted routes",
+			func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+				is := is.New(t)
+
+				err := wait.For(func() (done bool, err error) {
+					hc := &http.Client{}
+
+					req, err := http.NewRequest(
+						http.MethodGet,
+						fmt.Sprintf("%s%s", gatewayLocalhost, apiDefListenPath+"/randomNonWhiteListedPath"),
+						nil,
+					)
+					is.NoErr(err)
+					req.Header.Add("Content-type", "application/json")
+
+					resp, err := hc.Do(req)
+					is.NoErr(err)
+
+					if resp.StatusCode != errForbiddenResponseCode {
+						return false, nil
+					}
+
+					return true, nil
+				}, wait.WithTimeout(defaultTimeout))
+				is.NoErr(err)
+
+				return ctx
+			}).Feature()
+
+	testenv.Test(t, adCreate)
+}
