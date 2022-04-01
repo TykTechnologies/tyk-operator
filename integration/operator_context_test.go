d0b58bffd449a3308b6876453186468e16f8464a
andrei-tyk <andrei@tyk.io>
2022-04-01 17:46:00 +0300

    Adding timeout to integration test7000

--- integration/operator_context_test.go
+++ integration/operator_context_test.go
@@ -22,7 +22,7 @@ import (
 const (
 	testApiDef       = "test-http"
 	testOperatorCtx  = "mycontext"
-	gatewayLocalhost = "http://localhost:3000"
+	gatewayLocalhost = "http://localhost:7200"
 	//dashboardLocalHost = "http://localhost:7200"
 )
 
