8f6ed86b914ae6532cb6ddb31846af39a3761781
Burak Sekili <buraksekili@gmail.com>
2022-05-25 12:30:23 +0300

    [TT-5320] - Support different namespaces for SubGraph (#466)
    * Add support for the use of SubGraph CRDs located in different namespaces
    
    * update example yaml files for multiple namespace support
    
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>
    
    * remove helper function
    
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- api/model/supergraph.go
+++ api/model/supergraph.go
@@ -2,7 +2,7 @@ package model
 
 // SuperGraphSpec defines the desired state of SuperGraph
 type SuperGraphSpec struct {
-	SubgraphsRefs []string `json:"subgraphs_refs"`
-	MergedSDL     string   `json:"merged_sdl,omitempty"`
-	Schema        string   `json:"schema,omitempty"`
+	SubgraphRefs []Target `json:"subgraph_refs"`
+	MergedSDL    string   `json:"merged_sdl,omitempty"`
+	Schema       string   `json:"schema,omitempty"`
 }
