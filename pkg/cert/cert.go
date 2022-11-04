782a3daa238e3debe0f70502f0b2b5776dd9ac35
Komal Sukhani <komaldsukhani@gmail.com>
2022-11-04 13:02:31 +0530

    Return error if certificate is invalid

--- pkg/cert/cert.go
+++ pkg/cert/cert.go
@@ -4,15 +4,21 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/pem"
+	"errors"
 )
+
+var ErrInvalidCert = errors.New("certificate is invalid")
 
 func HexSHA256(cert []byte) string {
 	certSHA := sha256.Sum256(cert)
 	return hex.EncodeToString(certSHA[:])
 }
 
-func CalculateFingerPrint(data []byte) string {
-	// TODO: publicKey might be returned as nil. if that's the case, the program panics.
+func CalculateFingerPrint(data []byte) (string, error) {
 	publicKey, _ := pem.Decode(data)
-	return HexSHA256(publicKey.Bytes)
+	if publicKey != nil {
+		return HexSHA256(publicKey.Bytes), nil
+	}
+
+	return "", ErrInvalidCert
 }
