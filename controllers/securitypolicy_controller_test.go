0e0605bec6f5e44a597aed2396d13fcf320b970b
Komal Sukhani <komaldsukhani@gmail.com>
2023-01-09 17:04:41 +0530

    [TT-5089] Add new field in security policy status (#547)
    * Add a `linkedAPIs` field in status of security policy
    
    * Store list of linked APIs in the status of policy
    
    * Set back MID
    
    * Remove tests for updateStatusOfLinkedApis
    
    * Add tests for security policy
    
    * Remove redundant checks
    
    * Fix lint issues
    
    * Fix tests
    
    * Fix lint issues
    
    * Change arguement sequence
    
    * Do not run security policy tests for ce mode
    
    * Increase timeout period
    
    * Fix tests
    
    * Fix lint issues
    
    * Fix client mtls test
    
    * Update changelog
    
    * Make an entry of the bug in the changelog
    
    * Update log messages
    
    * Rename var
    
    * Fix lint issue
    
    * Fix tests
    
    * Update policy MID

---
+++ controllers/securitypolicy_controller_test.go
@@ -0,0 +1,92 @@
+package controllers
+
+import (
+	"context"
+	"testing"
+
+	"github.com/TykTechnologies/tyk-operator/api/model"
+	tykv1 "github.com/TykTechnologies/tyk-operator/api/v1alpha1"
+	"github.com/matryer/is"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func TestUpdatePolicyStatus(t *testing.T) {
+	is := is.New(t)
+
+	api1 := &tykv1.ApiDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "api1",
+		},
+		Spec: tykv1.APIDefinitionSpec{
+			APIDefinitionSpec: model.APIDefinitionSpec{
+				Name:            "api1",
+				Active:          true,
+				UseStandardAuth: true,
+			},
+		},
+	}
+
+	api2 := &tykv1.ApiDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "api2",
+		},
+		Spec: tykv1.APIDefinitionSpec{
+			APIDefinitionSpec: model.APIDefinitionSpec{
+				Name:            "api2",
+				Active:          true,
+				UseStandardAuth: true,
+			},
+		},
+	}
+
+	tests := map[string]struct {
+		Policy     *tykv1.SecurityPolicy
+		LinkedAPIs []model.Target
+	}{
+		"emtpy acess rights policy": {
+			Policy: &tykv1.SecurityPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "policy without access rights",
+				},
+			},
+		},
+		"policy with access rights": {
+			Policy: &tykv1.SecurityPolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "policy with access rights",
+				},
+				Spec: tykv1.SecurityPolicySpec{
+					Name: "policy with access rights",
+					AccessRightsArray: []*tykv1.AccessDefinition{
+						{
+							Name: "api1",
+						},
+						{
+							Name: "api2",
+						},
+					},
+				},
+			},
+			LinkedAPIs: []model.Target{{Name: "api1"}, {Name: "api2"}},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			c, err := NewFakeClient([]runtime.Object{api1, api2, test.Policy})
+			is.NoErr(err)
+
+			r := &SecurityPolicyReconciler{
+				Client: c,
+				Log:    log.NullLogger{},
+			}
+
+			err = r.updatePolicyStatus(context.TODO(), test.Policy)
+			is.NoErr(err)
+
+			is.Equal(test.Policy.Status.LinkedAPIs, test.LinkedAPIs)
+		})
+	}
+}
