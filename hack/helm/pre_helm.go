b32d57c85b17365caec0d0506570cabb56671d98
Zaid Albirawi <zaid@tyk.io>
2023-03-15 08:21:28 -0400

    [TT-6478] Fix control-plane label so that it does not conflict with other tools (#583)
    * Fix control-plane label so that it does not conflict with other tools
    
    * Update changelog
    
    ---------
    
    Co-authored-by: Pranshu <104971506+singhpr@users.noreply.github.com>

--- hack/helm/pre_helm.go
+++ hack/helm/pre_helm.go
@@ -42,7 +42,7 @@ const namespace = `apiVersion: v1
 kind: Namespace
 metadata:
   labels:
-    control-plane: controller-manager
+    control-plane: tyk-operator-controller-manager
   name: RELEASE_NAMESPACE
 ---`
 
