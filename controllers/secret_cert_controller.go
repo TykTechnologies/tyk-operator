e44e0514c4fc7732339c0cac070b56cc0045cb7e
Komal Sukhani <komaldsukhani@gmail.com>
2023-04-14 16:17:09 +0530

    Fix error handling

--- controllers/secret_cert_controller.go
+++ controllers/secret_cert_controller.go
@@ -150,6 +150,7 @@ func (r *SecretCertReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 
 				if err != nil {
 					log.Error(err, "Unable to update API Definition with cert id", "apiID", apiDefList.Items[idx].Spec.APIID)
+					return ctrl.Result{Requeue: true}, nil
 				}
 
 				log.Info("api def updated successfully")
