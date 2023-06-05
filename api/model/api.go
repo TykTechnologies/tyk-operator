8316615bc3f4f86232bad6946a9a0bed2b68e2b5
Burak Sekili <buraksekili@gmail.com>
2023-06-05 13:11:55 +0300

    Update validation enum for signing methods
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- api/model/api.go
+++ api/model/api.go
@@ -671,7 +671,7 @@ type APIDefinitionSpec struct {
 	EnableCoProcessAuth *bool `json:"enable_coprocess_auth,omitempty"`
 
 	// JWTSigningMethod algorithm used to sign jwt token
-	// +kubebuilder:validation:Enum=rsa;hmac;ecdsa
+	// +kubebuilder:validation:Enum="";rsa;hmac;ecdsa
 	JWTSigningMethod *string `json:"jwt_signing_method,omitempty"`
 
 	// JWTSource Must either be a base64 encoded valid RSA/HMAC key or a url to a
