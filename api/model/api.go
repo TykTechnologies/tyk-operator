3624625d885514b3e562f71636c1a99ad395584d
Burak Sezer <burak.sezer.developer@gmail.com>
2023-12-21 16:15:48 +0300

    [TT-10477, TT-10633] Add GraphQLIntrospectionConfig struct (#662)
    * [TT-10477] Add GraphQLIntrospectionConfig struct
    
    * [TT-10477] Add auto-generated files
    
    * [TT-10477] Add `omitempty` tag to Introspection field.
    
    * [TT-10477] Update CHANGELOG.md
    
    * [TT-10477] Add `omitempty` tag to GraphQLIntrospectionConfig.Disabled field.

--- api/model/api.go
+++ api/model/api.go
@@ -1163,6 +1163,13 @@ type GraphQLConfig struct {
 
 	// Supergraph holds the configuration for a GraphQL federation supergraph.
 	Supergraph GraphQLSupergraphConfig `json:"supergraph,omitempty"`
+
+	// Introspection holds the configuration for GraphQL Introspection
+	Introspection GraphQLIntrospectionConfig `json:"introspection,omitempty"`
+}
+
+type GraphQLIntrospectionConfig struct {
+	Disabled bool `json:"disabled,omitempty"`
 }
 
 type GraphQLProxyConfig struct {
