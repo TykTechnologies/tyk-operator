50cc64f9465c16d18a1f2a7bdbaa229d4d123429
Komal Sukhani <komaldsukhani@gmail.com>
2024-01-09 13:38:30 +0530

    Fix linter error

--- api/model/api.go
+++ api/model/api.go
@@ -846,8 +846,8 @@ type APIDefinitionSpec struct {
 	// records, raw request and responses recorded by Tyk at the gateway level
 	// +optional
 	AnalyticsPlugin AnalyticsPluginConfig `json:"analytics_plugin,omitempty"`
-  
-  // +optional
+
+	// +optional
 	// +nullable
 	DetailedTracing *bool `json:"detailed_tracing,omitempty"`
 }
