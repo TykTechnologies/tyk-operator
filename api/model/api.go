0fd452b8b8357d52705f715653fe49e83bd06801
Burak Sekili <buraksekili@gmail.com>
2024-01-10 10:53:32 +0300

    [TT-10633] Add GraphQLIntrospectionConfig struct to ApiDefinition CRD (#670)
    * [TT-10633] Add GraphQLIntrospectionConfig struct to ApiDefinition CRD"
    
    * run gofmt
    
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>
    
    ---------
    
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- api/model/api.go
+++ api/model/api.go
@@ -1167,6 +1167,13 @@ type GraphQLConfig struct {
 
 	// Supergraph holds the configuration for a GraphQL federation supergraph.
 	Supergraph GraphQLSupergraphConfig `json:"supergraph,omitempty"`
+
+	// Introspection holds the configuration for GraphQL Introspection
+	Introspection GraphQLIntrospectionConfig `json:"introspection,omitempty"`
+}
+
+type GraphQLIntrospectionConfig struct {
+	Disabled bool `json:"disabled,omitempty"`
 }
 
 type GraphQLProxyConfig struct {
